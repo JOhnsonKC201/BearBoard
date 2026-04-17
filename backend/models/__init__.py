492ca3214c605c2803292259af4e9931abcdfa8d
JOhnsonKC201 <johnsonkc201@gmail.com>
2026-04-17 11:43:22 -0400

    Set up Alembic with initial migration for all tables

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,2 +1,6 @@
 from models.user import User
 from models.post import Post
+from models.vote import Vote
+from models.comment import Comment
+from models.event import Event
+from models.group import Group
